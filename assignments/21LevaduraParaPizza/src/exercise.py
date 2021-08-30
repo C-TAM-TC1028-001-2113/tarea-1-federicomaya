0c7526dffc2e620d9691d9ca1b3064b26b47d57b
federicomaya <89592259+federicomaya@users.noreply.github.com>
2021-08-30 18:03:55 -0500

    Ejercicio Levadura para pizaa completado

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
+    #Federico Maya Santander A01411914
     # escribe tu código abajo de esta línea
-    pass
+    
+    gramos = float(input("Dame la harina en gramos: "))
+    gramos_harina = (gramos * 50) / 1000
+    print("Necesitas estos gramos de levadura:", gramos_harina)
+    
 
 
 if __name__ == '__main__':
