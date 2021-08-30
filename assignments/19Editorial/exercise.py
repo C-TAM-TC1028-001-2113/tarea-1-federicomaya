c96886819fd608cc8dc6acedabd7e9d82648ee44
federicomaya <89592259+federicomaya@users.noreply.github.com>
2021-08-29 22:56:43 -0500

    Add files via upload
    ejercicio editorial completado

---
+++ assignments/19Editorial/exercise.py
@@ -0,0 +1,15 @@
+def main():
+    #Federico Maya Santander A01411914
+    # escribe tu código abajo de esta línea
+    
+    import math
+    num_palabras = int(input("Dame el número de palabras: "))
+    num_paginas = math.ceil(num_palabras/475)
+    costo_publicacion = (num_paginas)*60
+    precio_total = (costo_publicacion)*.9
+    print("El costo de la publicacion es:", precio_total)
+    
+
+
+if __name__ == '__main__':
+    main()
