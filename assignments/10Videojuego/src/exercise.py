a87f033dc699cbf32e3cfabccf1add272bca4efe
federicomaya <89592259+federicomaya@users.noreply.github.com>
2021-08-30 18:01:49 -0500

    Ejercicio videojuego completado

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
+    # Federico Maya Santander A01411914
     # escribe tu código abajo de esta línea
-    pass
+
+    nuevo = int(input("Escribe la cantidad de juegos nuevos: "))*1000
+    usado = int(input("Escribe la cantidad de juegos usados: "))*350
+    total = nuevo + usado
+    print("El total de la compra es de:", total)
+    
 
 
 if __name__ == '__main__':
