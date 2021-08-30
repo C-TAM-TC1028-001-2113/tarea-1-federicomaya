e19ac9d388ca7befd3f9993bad7a9d4adcd7b3e4
federicomaya <89592259+federicomaya@users.noreply.github.com>
2021-08-29 22:35:06 -0500

    Add files via upload
    Ejercicio videojuegos completado

---
+++ assignments/10Videojuego/exercise.py
@@ -0,0 +1,13 @@
+def main():
+    # Federico Maya Santander A01411914
+    # escribe tu código abajo de esta línea
+
+    nuevo = int(input("Escribe la cantidad de juegos nuevos: "))*1000
+    usado = int(input("Escribe la cantidad de juegos usados: "))*350
+    total = nuevo + usado
+    print("El total de la compra es de:", total)
+    
+
+
+if __name__ == '__main__':
+    main()
